172bfc4642fd1e1bd89b24e4a5436e22439b74e6
Diana Cismaru <cismaru_diana02@yahoo.com>
2024-12-13 01:52:56 +0200

    Create exception handler

---
+++ src/main/java/com/example/t2scd/application/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.t2scd.application.configuration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<?> handle(IllegalArgumentException e) {
+		System.out.println(e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<?> handle(NoSuchElementException e) {
+		System.out.println(e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handle(Exception e) {
+		System.out.println(e.getMessage());
+		return ResponseEntity.status(HttpStatus.CONFLICT).build();
+	}
+}
